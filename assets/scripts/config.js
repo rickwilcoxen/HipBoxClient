9ab51fe6e20f660d4c61a39352074d8c595a4162
rickwilcoxen <rickwilcoxen95@gmail.com>
2020-05-04 18:07:16 -0400

    Fixed typo

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://desolate-bayou-55444.herokuapp.com/',
+  production: 'https://desolate-bayou-55444.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
